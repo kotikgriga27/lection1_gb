8319a66626e0c6aa50671ff37e38b536924d1edd
Daria <kotikgriga0027@gmail.com>
2023-02-02 16:06:41 +0300

    file ex_5

---
+++ ex_5/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 10); // от 1 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
