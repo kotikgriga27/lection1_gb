b98108061f16a314df669e51feb4bf5b9d17ee6b
Daria <kotikgriga0027@gmail.com>
2023-02-02 16:28:36 +0300

    file ex_6

---
+++ ex_6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша") {
+    Console.WriteLine("Ура! Это же МАША!");
+} else {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);   
+}
